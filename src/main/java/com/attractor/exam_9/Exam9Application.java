b57f787c29dcb05ae778e73367850654e49a8991
Semma <semm1998>
2020-05-23 13:14:38 +0600

    Initial commit

---
+++ src/main/java/com/attractor/exam_9/Exam9Application.java
@@ -0,0 +1,13 @@
+package com.attractor.exam_9;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Exam9Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Exam9Application.class, args);
+    }
+
+}
---
+++ src/test/java/com/attractor/exam_9/Exam9ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.attractor.exam_9;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Exam9ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
